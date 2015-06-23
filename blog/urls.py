9a2b9b9634f379b46d2ea141405d42d3f02aada0
Raymond <rupiray@yahoo.com>
2015-06-23 19:28:05 +0100

    more views added and README file

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from . import views
 
 
 urlpatterns = [
 	url(r'^$', views.post_list), # an empty string will match
-	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail), # post detail
+	url(r'^post/new/$', views.post_new, name='post_new'), # new post
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'), # edit post
 ]
